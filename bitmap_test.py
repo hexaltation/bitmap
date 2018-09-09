2061cf45085ab272a07b447ed7bd17a8c092ee2c
Grégoire Cutzach <caztuc@hotmail.com>
2018-09-09 11:39:33 +0200

    Tests added to bitmap_test.py

--- bitmap_test.py
+++ bitmap_test.py
@@ -26,3 +26,22 @@ BmpFile(green, 1, 1, 'test_output/green.bmp').write()
 # Blue 1x1px bmp file
 blue = bytearray([255, 0, 0, 0, 0, 0])
 BmpFile(blue, 1, 1, 'test_output/blue.bmp').write()
+
+# 6x4px french flag file
+hexblue = bytearray.fromhex('FF0000')
+hexwhite = bytearray.fromhex('FFFFFF')
+hexred = bytearray.fromhex('0000FF')
+height = 4
+width = 6
+flagdata = bytearray()
+for h in range(height):
+    for w in range(width + ((width * 3) % 4)):
+        if w < 2:
+            flagdata.extend(hexblue)
+        elif w < 4:
+            flagdata.extend(hexwhite)
+        elif w < 6:
+            flagdata.extend(hexred)
+        else:
+            flagdata.extend(bytearray(1))
+BmpFile(flagdata, width, height, 'test_output/flag.bmp').write()
